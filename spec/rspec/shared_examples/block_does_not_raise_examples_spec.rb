f264fde81a67f72f361906356cad58d11053bcab
Peter Boling <peter.boling@gmail.com>
2024-06-23 16:28:21 +0700

    🚨 Linting

--- spec/rspec/shared_examples/block_does_not_raise_examples_spec.rb
+++ spec/rspec/shared_examples/block_does_not_raise_examples_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "block_is_expected to not raise works" do
+RSpec.describe "block_is_expected to not raise works" do # rubocop:disable RSpec/DescribeClass:
   subject { Integer("1") }
 
   it_behaves_like "block_is_expected to not raise"
